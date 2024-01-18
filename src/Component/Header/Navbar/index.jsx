d69aa41c429a793b4930a54ce10d9391b0567e99
M. Mutahar <mmutahar38@gamil.com>
2024-01-18 11:10:29 +0500

    resposive navbar setting show hide menu-bar in tablet and mobile screen

--- src/Component/Header/Navbar/index.jsx
+++ src/Component/Header/Navbar/index.jsx
@@ -1,8 +1,10 @@
-import react from "react"
+import react, { useState } from "react"
 import { Link } from "react-router-dom"
 import { NavLink } from "react-router-dom"
 
 const Navbar=()=>{
+
+  const [menue,setMenue]=useState(false);
 
     return <>
     
@@ -37,19 +39,19 @@ const Navbar=()=>{
           </li>
         </ul>
       </div>
-      <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+      <button onClick={ () => setMenue(menue => !menue) } data-collapse-toggle="navbar-user" type="button" className=" inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400  dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
   </div>
-  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+  <div className={`${menue ? '': 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1`} id="navbar-user">
     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
-        <Link className="block py-2 px-3 text-white bg-[#BF1017] rounded md:bg-transparent md:text-[#BF1017] md:p-0 md:dark:text-blue-500" aria-current="page">
+        <Link to="/dashboard" className="block py-2 px-3 text-black  hover:text-white hover:bg-[#BF1017] md:hover:bg-transparent rounded md:bg-transparent md:p-0">
           <NavLink to="/dashboard" className={({ isActive, }) =>
- isActive ? "text-[#BF1017]" : "text-black"
+ isActive ? "md:text-[#BF1017]" : "text-black"
   }>
           
           Home
@@ -57,9 +59,9 @@ const Navbar=()=>{
           </Link>
       </li>
       <li>
-        <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#BF1017] md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <Link to="/about" className="block py-2 px-3 text-black hover:text-white rounded hover:bg-[#BF1017] md:hover:bg-transparent md:hover:text-[#BF1017] md:p-0 dark:text-white md:dark:hover:text-white  dark:border-gray-700">
            <NavLink to="/about" className={({ isActive, }) =>
- isActive ? "text-[#BF1017]" : "text-black"
+ isActive ? "md:text-[#BF1017]" : "md:text-black"
   }>
           
           About
@@ -68,9 +70,9 @@ const Navbar=()=>{
           </Link>
       </li>
       <li>
-        <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#BF1017] md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <Link to="/services" className="block py-2 px-3 text-black hover:text-white  rounded hover:bg-[#BF1017] md:hover:bg-transparent md:hover:text-[#BF1017] md:p-0 dark:text-white md:dark:hover:text-white  dark:border-gray-700">
            <NavLink to="/services" className={({ isActive, }) =>
- isActive ? "text-[#BF1017]" : "text-black"
+ isActive ? "md:text-[#BF1017]" : "md:text-black"
   }>
           
           Services
@@ -78,7 +80,7 @@ const Navbar=()=>{
           </Link>
       </li>
       {/* <li>
-        <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#BF1017] md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#BF1017] md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:border-gray-700">
            <NavLink to="/dashboard" className={({ isActive, }) =>
  isActive ? "text-[#BF1017]" : "text-black"
   }>
@@ -88,9 +90,9 @@ const Navbar=()=>{
           </Link>
       </li> */}
       <li>
-        <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#BF1017] md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <Link to="/contact-us" className="block py-2 px-3 text-black hover:text-white rounded hover:bg-[#BF1017] md:hover:bg-transparent md:hover:text-[#BF1017] md:p-0 dark:text-white md:dark:hover:text-white  dark:border-gray-700">
            <NavLink to="/contact-us" className={({ isActive, }) =>
- isActive ? "text-[#BF1017]" : "text-black"
+ isActive ? "md:text-[#BF1017]" : "md:text-black"
   }>
           
           Contact
